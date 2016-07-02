9200c3f8714752367ab15b1d50d59128fc4eef06
iwood25 <petr.ryabokon@gmail.com>
2016-07-02 17:12:24 +0300

    Create mobile menu, adapted top line

--- ContentBuild/js/main.js
+++ ContentBuild/js/main.js
@@ -12,6 +12,30 @@
 		heightDetect();
 	});
 
+	// create mobile menu
+
+	$(".mobile__menu").append("<div class='my-menu'>");
+	$(".header__menu").clone().appendTo(".my-menu");
+	$(".my-menu").find("ul").attr("class", "mobile__menu-list");
+	$(".my-menu").find("li").attr("class", "mobile__menu-item");
+	$(".my-menu").find("a").attr("class", "mobile__menu-link");
+
+
+	// sandwich menu
+	$(".toggle-menu, .menu_item").click(function () {
+		$(".sandwich").toggleClass("active");
+	});
+
+	// mobile menu
+	$(".toggle-menu").click(function () {
+		if ($(".mobile__menu").is(":visible")) {
+			$(".mobile__menu").fadeOut(600);
+		} else {
+			$(".mobile__menu").fadeIn(600);
+		};
+	});
+
+
 })
 
 $(window).load(function () {
