8e77dc02efac717dfcf2bacc61b8b221c1ae6c35
iwood25 <petr.ryabokon@gmail.com>
2016-07-04 00:58:19 +0300

    Finished block header, installed : mouseIcon

--- ContentBuild/js/main.js
+++ ContentBuild/js/main.js
@@ -14,27 +14,37 @@
 
 	// create mobile menu
 
-	$(".mobile__menu").append("<div class='my-menu'>");
-	$(".header__menu").clone().appendTo(".my-menu");
-	$(".my-menu").find("ul").attr("class", "mobile__menu-list");
-	$(".my-menu").find("li").attr("class", "mobile__menu-item");
-	$(".my-menu").find("a").attr("class", "mobile__menu-link");
+	function createMenu() {
 
+		$(".mobile__menu").append("<div class='my-menu'>");
+		$(".header__menu").clone().appendTo(".my-menu");
+		$(".my-menu").find("ul").attr("class", "mobile__menu-list");
+		$(".my-menu").find("li").attr("class", "mobile__menu-item");
+		$(".my-menu").find("a").attr("class", "mobile__menu-link");
+
+		// when we click on the link - menu is fadeout
+		$(".my-menu a").click(function () {
+			$(".mobile__menu").fadeOut(600);
+			$(".sandwich").toggleClass("active");
+			$(".mobile__menu").empty();
+		});
+
+	};
 
 	// sandwich menu
-	$(".toggle-menu, .menu_item").click(function () {
-		$(".sandwich").toggleClass("active");
-	});
-
-	// mobile menu
 	$(".toggle-menu").click(function () {
+		$(".sandwich").toggleClass("active");
+
 		if ($(".mobile__menu").is(":visible")) {
 			$(".mobile__menu").fadeOut(600);
+			$(".mobile__menu").empty();
 		} else {
 			$(".mobile__menu").fadeIn(600);
+			createMenu(); // create mobile menu
+			$(".mobile__menu li a").addClass("fadeInUp animated");
 		};
 	});
-
+	
 
 })
 
